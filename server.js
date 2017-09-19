cd7b1d98b95d89c4601fee2be684bff887695c54
bolu ajibs <bolu.ajibawo@gmail.com>
2017-09-19 10:11:23 +0100

    added EJS and setup home page and layouts

--- server.js
+++ server.js
@@ -8,11 +8,20 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+const expressLayouts = require('express-ejs-layouts');
+
+
+// tell express where to look for static assets
+app.use(express.static(`${__dirname}/public`));
+
+// set ejs as templating engine
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Voting App');
+  res.render('pages/home');
 });
-
 
 app.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
